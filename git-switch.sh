29368977613f678db4a47b1148c6a49fca385eec
Austin Steady <asteady23@gmail.com>
2019-01-28 19:30:35 -0800

    init

---
+++ git-switch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo "Need to supply a branch"
+    exit
+fi
+
+current_branch=`git branch | grep \* | cut -d ' ' -f2`
+git stash save $current_branch
+git checkout $1
+git stash apply stash@{$1}
+git stash drop stash@{$1}
+
+echo "Switched to branch $1"
