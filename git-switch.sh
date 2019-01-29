319b2c2ae82a294b4fb36d12dfbd5f1f244e41f7
Austin Steady <asteady23@gmail.com>
2019-01-28 20:44:09 -0800

    if statement style

--- git-switch.sh
+++ git-switch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-then
+if [ $# -eq 0 ] ; then
     echo "Need to supply a branch"
     exit
 fi
@@ -10,13 +9,9 @@ current_branch=`git branch | grep \* | cut -d ' ' -f2`
 git stash push -m $current_branch
 git checkout $1
 
-# search git stash list for the matching branch name (and it has to be *exact*)
-# get the number index for that stash
-
 grep="^On $1: $1$"
 stash_n=`git stash list --max-count=1 --grep="$grep" | cut -f1 -d":"`
-if [[ -n "$stash_n" ]]
-then
+if [[ -n "$stash_n" ]] ; then
     git stash apply $stash_n
     git stash drop $stash_n
 else
